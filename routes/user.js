016907548380ae52d365a70cad8652afc0e0ae03
Bhargav1331 <123548291+Bhargav1331@users.noreply.github.com>
2023-02-01 15:10:11 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -6,7 +6,18 @@ const { auth } = require('../middleware/auth')
 router.get('/', first);
 router.post('/', login)
 router.get('/dashboard', auth, dashboard)
-
+router.get('/myprofile', auth, myprofile)
+router.post('/myprofile', auth, myprofile)
+router.get('/changepassword', auth, changepassword)
+router.post('/changepassword', auth, changepassword)
+router.get('/issuedbooks', auth, issuedbooks)
+router.get('/adminlogin', auth, adminlogin)
+router.get('/signup', signup)
+router.post('/signup', signup)
+router.get('/forgotpassword', forgotpassword)
+router.post('/forgotpassword', forgotpassword)
+router.get('/logout', auth, logout)
+router.post('/check_availability', check_availability)
 
 
 module.exports = router
